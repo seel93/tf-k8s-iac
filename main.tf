cf55a93940add9b46a8e3a18b31d80f12a253cca
Øystein Seel <oyse@ysteins-MacBook-Pro.local>
2024-02-19 08:41:39 +0100

    remove workflow & minor change to cronojob

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = "${var.resource_group_name_prefix}-aks-mvp-15-01"
+  name     = "${var.resource_group_name_prefix}${var.resource_group_name_suffix}"
 }
 
 resource "azurerm_container_registry" "acr" {
--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "resource_group_name_suffix" {
+  type = string
+  default = "-aks-mvp-15-01"
+}
